0a54dcfb13ce1edf32cf5a85c37cb69e68af5e7f
JohnnySilvaa <johnnysilva_estagio@hotmail.com>
2019-08-07 10:50:40 -0300

    header

--- src/app/layout/sidenav/sidenav.component.ts
+++ src/app/layout/sidenav/sidenav.component.ts
@@ -11,16 +11,26 @@ export class SidenavComponent implements OnInit {
   
   constructor(private titleService: Title, private colorPicker: ColorPickerService){}
 
+  
+
   ngOnInit() {
   }
 
   getTitle(){
-    return this.titleService.getTitle()
+    return this.titleService.getTitle();
+  }
+
+  getColorTheme(){
+    return this.colorPicker.getColorClass().value;
+  }
+
+  getColorHeader(){
+    return `header-${this.getColorTheme()}`;
   }
 
   pickColor(color: string){
     let colorTheme = '';
-
+    
     if(color !== ''){
       colorTheme = `-${color}`
     }
@@ -28,4 +38,5 @@ export class SidenavComponent implements OnInit {
       `app-theme${colorTheme}`
     )
   }
+
 }
