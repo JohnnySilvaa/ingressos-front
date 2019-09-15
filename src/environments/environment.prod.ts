2472f5359b5ec16ac872cf3f4c1f3b479f495e5a
Johnny Silva <johnnysilva.dev@gmail.com>
2019-09-15 14:01:10 -0300

    config cloudbuild

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,6 +3,7 @@ export const environment = {
 
   firebaseConfig: {
 
+
     apiKey: '<API_KEY>',
     authDomain: '<PROJECT_ID>.firebaseapp.com',
     databaseURL: 'https://<DATABASE_NAME>.firebaseio.com',
